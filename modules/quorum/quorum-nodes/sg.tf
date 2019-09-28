4aa4e1ce8f0958ddc3105ab890cf7b77ab20fa39
Charles Hsiao <charles_hsiao@maicoin.com>
2019-09-28 15:01:38 +0800

    Init quorum nodes

---
+++ modules/quorum/quorum-nodes/sg.tf
@@ -0,0 +1,100 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUPS
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "aws_vpc" "quorum-vpc" {
+  tags = {
+    Name = "${var.environment}-${var.project}-vpc"
+  }
+}
+
+# EC2
+resource "aws_security_group" "quorum-nodes-ct" {
+  name_prefix = "${var.environment}-${var.project}-${var.role}-ct-"
+  vpc_id      = "${data.aws_vpc.quorum-vpc.id}"
+
+  tags = "${merge(map(
+      "Name", "${var.environment}-${var.project}-${var.role}-ct",
+      "BuiltWith", "Terraform",
+    ), var.extra_tags)}"
+}
+
+resource "aws_security_group_rule" "egress-quorum-nodes-ct" {
+  type              = "egress"
+  security_group_id = "${aws_security_group.quorum-nodes-ct.id}"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress-quorum-nodes-ct-ssh" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.quorum-nodes-ct.id}"
+
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "quorum-nodes" {
+  name_prefix = "${var.environment}-${var.project}-${var.role}-"
+  vpc_id      = "${data.aws_vpc.quorum-vpc.id}"
+
+  tags = "${merge(map(
+      "Name", "${var.environment}-${var.project}-${var.role}",
+      "BuiltWith", "Terraform",
+    ), var.extra_tags)}"
+}
+
+resource "aws_security_group_rule" "egress-quorum-nodes" {
+  type              = "egress"
+  security_group_id = "${aws_security_group.quorum-nodes.id}"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress-quorum-nodes-ssh" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.quorum-nodes.id}"
+
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  source_security_group_id = "${aws_security_group.quorum-nodes-ct.id}"
+}
+
+resource "aws_security_group_rule" "ingress-quorum-nodes-peer-tcp" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.quorum-nodes.id}"
+
+  from_port   = 30301
+  to_port     = 30351
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress-quorum-nodes-peer-udp" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.quorum-nodes.id}"
+
+  from_port   = 30301
+  to_port     = 30351
+  protocol    = "udp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress-quorum-nodes-rpc" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.quorum-nodes.id}"
+
+  from_port   = 8543
+  to_port     = 8593
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
